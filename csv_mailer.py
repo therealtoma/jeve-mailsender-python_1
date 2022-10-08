8a31c97e25132219baddd9794a88f4549a14eecd
andreramolivaz <a.ramolivaz@icloud.com>
2022-10-09 01:19:01 +0200

    Update csv_mailer.py
    Fixed 'smtp connection failed'

--- csv_mailer.py
+++ csv_mailer.py
@@ -1,11 +1,13 @@
 import smtplib
 import csv
 import time
+from datetime import datetime, timedelta
+
 # CONFIG
 CSVFILE = "recipients.csv"
 TEMPLATE = "mail_template.txt"
 
-SENDER = 'andre.ramolivaz@jeve.it'
+SENDER = ''
 SMTP_SERVER = 'smtp.gmail.com'
 SMTP_PORT = 587
 
@@ -17,14 +19,23 @@ SMTP_PASS = ''  # per autorizzazione,  SMTP psw
 # TEST
 DRY_RUN = 0  # non invia mail ma stampa errori nel caso in cui ce ne sarebbero potuti essere
 SAFE_MODE = 0  # invia mail al recipients al posto di csv
-RECIPIENTS = ['']
+RECIPIENTS = ['alberto.tomasin@gmail.com']
 
 template = open(TEMPLATE, "r")
 csvfile = open(CSVFILE, "r")
 
 mail_template = template.read()
 csv_reader = csv.DictReader(csvfile)  # add 'dialect="semicolon"' if necessary
+lines = len(list(csv_reader))
 
+print("Minuti stimati per inviare le mail: " + str((lines +(lines/50*120))/60))
+now = datetime.today()
+print("Ora attuale: "+ str(now))
+result_2 = now + timedelta(seconds=(lines +(lines/50*120)))
+print("Ora stimata di fine esecuzione: "+ str(result_2))
+
+csvfile = open(CSVFILE, "r")
+csv_reader = csv.DictReader(csvfile)  # add 'dialect="semicolon"' if necessary
 # Inizia sessione SMTP
 smtpresult = 0
 if not DRY_RUN:
@@ -37,15 +48,15 @@ if not DRY_RUN:
         session.ehlo()
     if AUTH_REQUIRED:
         session.login(SMTP_USER, SMTP_PASS)
-counter = 0
+counter = 1
 # Invio mail
 for row in csv_reader:
     if counter % 50 == 0:
-        print ("Chiudo sessione SMTP")
+        print("Chiudo sessione SMTP")
         session.close()
         print("E' tempo di dormire")
-        time.sleep(2);
-        print ("Riapro la sessione SMTP")
+        time.sleep(120);
+        print("Riapro la sessione SMTP")
         if not DRY_RUN:
             print("Sessione SMTP aperta")
             session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
@@ -65,8 +76,8 @@ for row in csv_reader:
     email = row["Email"]
     id = row["ID"]
     recipient = "\"" + givenname + " " + surname + "\" " + "<" + email + ">"
-
-    print("Invio mail a " + email)
+    time.sleep(1)
+    print(str(counter) + " - Invio mail a " + email)
     mssg = mail_template.replace("$NAME$", givenname + " " + surname)
     mssg = mssg.replace("$SENDER$", SENDER)
     mssg = mssg.replace("$RECIPIENT$", recipient)
